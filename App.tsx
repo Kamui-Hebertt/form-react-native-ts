82b376c3d4f00fc7561e5fc2068045ac64b139c8
Hebertt <heberttaferreira.com@gmail.com>
2023-03-20 02:03:21 -0300

    feat:chekcing name

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import FieldName from './containers/nameForm';
+import HereIsYourName from './containers/HereIsYourName';
 
 export default function App() {
+  const [nameinserted , setNameInserted] = useState<string>('');
+
+
+const changeName = (newName:string):void =>{
+  setNameInserted(newName);
+}
+const clearNameField = ():void =>{
+  setNameInserted('');
+}
   return (
     <View style={styles.container}>
-      <FieldName />
+        {nameinserted === '' && ( <FieldName onSend={changeName} /> )}
+        {nameinserted !== '' && (<HereIsYourName names={nameinserted} clear={clearNameField} />)}
     
       <StatusBar style="auto" />
     </View>
--- containers/nameForm.tsx
+++ containers/nameForm.tsx
@@ -1,18 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import {  Text, TextInput, View } from 'react-native';
+import { Text, TextInput, View, Button } from 'react-native';
 
 
-export default function FieldName() {
-  const [ name, setName ] = useState<string>('')
-  console.log(name)
+interface Props  {
+  onSend: (newName : string) => void
+}
+export default function FieldName({ onSend }: Props) {
+   const [name, setName] = useState<string>('');
+
+   const onClick = ():void => {
+    onSend(name);
+   }
+
   return (
     <View >
       <Text>What is your name?</Text>
-      <TextInput placeholder='Enter your name' 
-      value={name}
-      onChangeText={t =>setName(t)}/>
-      
+      <TextInput placeholder='Enter your name'
+        value={name}
+        onChangeText={t => setName(t)} />
+        <Button title="Submit" onPress={onClick} />
       <StatusBar style="auto" />
     </View>
   );
